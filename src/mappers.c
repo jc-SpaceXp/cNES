94b611e79a7f6a7c682f5076ffccabebf50925cd
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2022-08-23 09:13:22 +0100

    Removed commented out code from mappers src files

--- src/mappers.c
+++ src/mappers.c
@@ -232,22 +232,3 @@ static void mmc1_reg_write(Cpu6502* cpu, uint16_t addr, uint8_t val)
 		buffer = 0;
 	}
 }
-
-
-/* CNROM mapper */
-//void mapper_003(Cartridge* cart)
-//{
-	// NOT FINISHED
-	//uint8_t *PRG_ROM_BANK_1 = &cpu->mem[0x8000]; /* till BFFF */
-	//uint8_t *PRG_ROM_BANK_2 = &cpu->mem[0xC000]; /* till FFFF */
-	//if (cart->prg_rom.size == 16 * KiB) {
-		//memcpy(PRG_ROM_BANK_1, cart->prg_rom.data, 16 * KiB); /* First 16KiB */
-		//memcpy(PRG_ROM_BANK_2, PRG_ROM_BANK_1, 16 * KiB); /* Last 16KiB */
-	//} else {
-		//memcpy(PRG_ROM_BANK_1, cart->prg_rom.data, 32 * KiB);
-	//}
-	// Writing to PPU->RAM[0 to 1FFF]
-	//uint8_t *CHR_ROM_BANK = &PPU->RAM[0x0000]; /* till 1FFF */
-	//memcpy(CHR_ROM_BANK, cart->chr_rom.data, 8 * KiB);
-	//free(cart->prg_rom.data);
-//}
--- src/mappers.h
+++ src/mappers.h
@@ -7,6 +7,4 @@ void mapper_write(Cpu6502* cpu, uint16_t addr, uint8_t val);
 void change_nt_mirroring(Cpu6502* cpu);
 void init_mapper(Cartridge* cart, Cpu6502* cpu, Ppu2A03* ppu);
 
-//void mapper_003(struct Cartridge* cart); /* CROM mapper */
-
 #endif /* __MAPPERS__ */
