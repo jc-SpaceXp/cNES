ef873d4dbd60d44eacdf63d25bc2d2f3e2e8cf01
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2022-10-12 09:16:12 +0100

    Completed reverse opcode lut function (for unit tests)
    - Unit tests can now use this helper function to help test cpu related
      functions by providing an instruction and address mode to the
      functions arguments
    - This helper function is also unit tested

--- tests/cpu_tests.c
+++ tests/cpu_tests.c
@@ -99,7 +99,93 @@ int reverse_opcode_lut(char (*instruction)[4], AddressMode address_mode)
 	else if (!strncmp(*instruction, "ADC", 4) && (address_mode == ABSY)) { opcode = 0x79; }
 	else if (!strncmp(*instruction, "ADC", 4) && (address_mode == ABSX)) { opcode = 0x7D; }
 	else if (!strncmp(*instruction, "ROR", 4) && (address_mode == ABSX)) { opcode = 0x7E; }
-	// row 9 (verfied up to here)
+	// row 9
+	else if (!strncmp(*instruction, "STA", 4) && (address_mode == INDX)) { opcode = 0x81; }
+	else if (!strncmp(*instruction, "STY", 4) && (address_mode == ZP)) { opcode = 0x84; }
+	else if (!strncmp(*instruction, "STA", 4) && (address_mode == ZP)) { opcode = 0x85; }
+	else if (!strncmp(*instruction, "STX", 4) && (address_mode == ZP)) { opcode = 0x86; }
+	else if (!strncmp(*instruction, "DEY", 4) && (address_mode == IMP)) { opcode = 0x88; }
+	else if (!strncmp(*instruction, "TXA", 4) && (address_mode == IMP)) { opcode = 0x8A; }
+	else if (!strncmp(*instruction, "STY", 4) && (address_mode == ABS)) { opcode = 0x8C; }
+	else if (!strncmp(*instruction, "STA", 4) && (address_mode == ABS)) { opcode = 0x8D; }
+	else if (!strncmp(*instruction, "STX", 4) && (address_mode == ABS)) { opcode = 0x8E; }
+	// row 10
+	else if (!strncmp(*instruction, "BCC", 4) && (address_mode == REL)) { opcode = 0x90; }
+	else if (!strncmp(*instruction, "STA", 4) && (address_mode == INDY)) { opcode = 0x91; }
+	else if (!strncmp(*instruction, "STY", 4) && (address_mode == ZPX)) { opcode = 0x94; }
+	else if (!strncmp(*instruction, "STA", 4) && (address_mode == ZPX)) { opcode = 0x95; }
+	else if (!strncmp(*instruction, "STX", 4) && (address_mode == ZPY)) { opcode = 0x96; }
+	else if (!strncmp(*instruction, "TYA", 4) && (address_mode == IMP)) { opcode = 0x98; }
+	else if (!strncmp(*instruction, "STA", 4) && (address_mode == ABSY)) { opcode = 0x99; }
+	else if (!strncmp(*instruction, "TXS", 4) && (address_mode == IMP)) { opcode = 0x9A; }
+	else if (!strncmp(*instruction, "STA", 4) && (address_mode == ABSX)) { opcode = 0x9D; }
+	// row 11
+	else if (!strncmp(*instruction, "LDY", 4) && (address_mode == IMM)) { opcode = 0xA0; }
+	else if (!strncmp(*instruction, "LDA", 4) && (address_mode == INDX)) { opcode = 0xA1; }
+	else if (!strncmp(*instruction, "LDX", 4) && (address_mode == IMM)) { opcode = 0xA2; }
+	else if (!strncmp(*instruction, "LDY", 4) && (address_mode == ZP)) { opcode = 0xA4; }
+	else if (!strncmp(*instruction, "LDA", 4) && (address_mode == ZP)) { opcode = 0xA5; }
+	else if (!strncmp(*instruction, "LDX", 4) && (address_mode == ZP)) { opcode = 0xA6; }
+	else if (!strncmp(*instruction, "TAY", 4) && (address_mode == IMP)) { opcode = 0xA8; }
+	else if (!strncmp(*instruction, "LDA", 4) && (address_mode == IMM)) { opcode = 0xA9; }
+	else if (!strncmp(*instruction, "TAX", 4) && (address_mode == IMP)) { opcode = 0xAA; }
+	else if (!strncmp(*instruction, "LDY", 4) && (address_mode == ABS)) { opcode = 0xAC; }
+	else if (!strncmp(*instruction, "LDA", 4) && (address_mode == ABS)) { opcode = 0xAD; }
+	else if (!strncmp(*instruction, "LDX", 4) && (address_mode == ABS)) { opcode = 0xAE; }
+	// row 12
+	else if (!strncmp(*instruction, "BCS", 4) && (address_mode == REL)) { opcode = 0xB0; }
+	else if (!strncmp(*instruction, "LDA", 4) && (address_mode == INDY)) { opcode = 0xB1; }
+	else if (!strncmp(*instruction, "LDY", 4) && (address_mode == ZPX)) { opcode = 0xB4; }
+	else if (!strncmp(*instruction, "LDA", 4) && (address_mode == ZPX)) { opcode = 0xB5; }
+	else if (!strncmp(*instruction, "LDX", 4) && (address_mode == ZPY)) { opcode = 0xB6; }
+	else if (!strncmp(*instruction, "CLV", 4) && (address_mode == IMP)) { opcode = 0xB8; }
+	else if (!strncmp(*instruction, "LDA", 4) && (address_mode == ABSY)) { opcode = 0xB9; }
+	else if (!strncmp(*instruction, "TSX", 4) && (address_mode == IMP)) { opcode = 0xBA; }
+	else if (!strncmp(*instruction, "LDY", 4) && (address_mode == ABSX)) { opcode = 0xBC; }
+	else if (!strncmp(*instruction, "LDA", 4) && (address_mode == ABSX)) { opcode = 0xBD; }
+	else if (!strncmp(*instruction, "LDX", 4) && (address_mode == ABSY)) { opcode = 0xBE; }
+	// row 13
+	else if (!strncmp(*instruction, "CPY", 4) && (address_mode == IMM)) { opcode = 0xC0; }
+	else if (!strncmp(*instruction, "CMP", 4) && (address_mode == INDX)) { opcode = 0xC1; }
+	else if (!strncmp(*instruction, "CPY", 4) && (address_mode == ZP)) { opcode = 0xC4; }
+	else if (!strncmp(*instruction, "CMP", 4) && (address_mode == ZP)) { opcode = 0xC5; }
+	else if (!strncmp(*instruction, "DEC", 4) && (address_mode == ZP)) { opcode = 0xC6; }
+	else if (!strncmp(*instruction, "INY", 4) && (address_mode == IMP)) { opcode = 0xC8; }
+	else if (!strncmp(*instruction, "CMP", 4) && (address_mode == IMM)) { opcode = 0xC9; }
+	else if (!strncmp(*instruction, "DEX", 4) && (address_mode == IMP)) { opcode = 0xCA; }
+	else if (!strncmp(*instruction, "CPY", 4) && (address_mode == ABS)) { opcode = 0xCC; }
+	else if (!strncmp(*instruction, "CMP", 4) && (address_mode == ABS)) { opcode = 0xCD; }
+	else if (!strncmp(*instruction, "DEC", 4) && (address_mode == ABS)) { opcode = 0xCE; }
+	// row 14
+	else if (!strncmp(*instruction, "BNE", 4) && (address_mode == REL)) { opcode = 0xD0; }
+	else if (!strncmp(*instruction, "CMP", 4) && (address_mode == INDY)) { opcode = 0xD1; }
+	else if (!strncmp(*instruction, "CMP", 4) && (address_mode == ZPX)) { opcode = 0xD5; }
+	else if (!strncmp(*instruction, "DEC", 4) && (address_mode == ZPX)) { opcode = 0xD6; }
+	else if (!strncmp(*instruction, "CLD", 4) && (address_mode == IMP)) { opcode = 0xD8; }
+	else if (!strncmp(*instruction, "CMP", 4) && (address_mode == ABSY)) { opcode = 0xD9; }
+	else if (!strncmp(*instruction, "CMP", 4) && (address_mode == ABSX)) { opcode = 0xDD; }
+	else if (!strncmp(*instruction, "DEC", 4) && (address_mode == ABSX)) { opcode = 0xDE; }
+	// row 15
+	else if (!strncmp(*instruction, "CPX", 4) && (address_mode == IMM)) { opcode = 0xE0; }
+	else if (!strncmp(*instruction, "SBC", 4) && (address_mode == INDX)) { opcode = 0xE1; }
+	else if (!strncmp(*instruction, "CPX", 4) && (address_mode == ZP)) { opcode = 0xE4; }
+	else if (!strncmp(*instruction, "SBC", 4) && (address_mode == ZP)) { opcode = 0xE5; }
+	else if (!strncmp(*instruction, "INC", 4) && (address_mode == ZP)) { opcode = 0xE6; }
+	else if (!strncmp(*instruction, "INX", 4) && (address_mode == IMP)) { opcode = 0xE8; }
+	else if (!strncmp(*instruction, "SBC", 4) && (address_mode == IMM)) { opcode = 0xE9; }
+	else if (!strncmp(*instruction, "NOP", 4) && (address_mode == IMP)) { opcode = 0xEA; }
+	else if (!strncmp(*instruction, "CPX", 4) && (address_mode == ABS)) { opcode = 0xEC; }
+	else if (!strncmp(*instruction, "SBC", 4) && (address_mode == ABS)) { opcode = 0xED; }
+	else if (!strncmp(*instruction, "INC", 4) && (address_mode == ABS)) { opcode = 0xEE; }
+	// row 16
+	else if (!strncmp(*instruction, "BEQ", 4) && (address_mode == REL)) { opcode = 0xF0; }
+	else if (!strncmp(*instruction, "SBC", 4) && (address_mode == INDY)) { opcode = 0xF1; }
+	else if (!strncmp(*instruction, "SBC", 4) && (address_mode == ZPX)) { opcode = 0xF5; }
+	else if (!strncmp(*instruction, "INC", 4) && (address_mode == ZPX)) { opcode = 0xF6; }
+	else if (!strncmp(*instruction, "SED", 4) && (address_mode == IMP)) { opcode = 0xF8; }
+	else if (!strncmp(*instruction, "SBC", 4) && (address_mode == ABSY)) { opcode = 0xF9; }
+	else if (!strncmp(*instruction, "SBC", 4) && (address_mode == ABSX)) { opcode = 0xFD; }
+	else if (!strncmp(*instruction, "INC", 4) && (address_mode == ABSX)) { opcode = 0xFE; }
 
 	return opcode;
 }
@@ -122,6 +208,24 @@ START_TEST (test_strcmp_reverse_opcode_lut)
 	ck_assert_int_eq(0x50, reverse_opcode_lut(ins, REL));
 	strncpy((char*) ins, "JMP", 4);
 	ck_assert_int_eq(0x6C, reverse_opcode_lut(ins, IND));
+	strncpy((char*) ins, "ADC", 4);
+	ck_assert_int_eq(0x79, reverse_opcode_lut(ins, ABSY));
+	strncpy((char*) ins, "STY", 4);
+	ck_assert_int_eq(0x8C, reverse_opcode_lut(ins, ABS));
+	strncpy((char*) ins, "STA", 4);
+	ck_assert_int_eq(0x91, reverse_opcode_lut(ins, INDY));
+	strncpy((char*) ins, "LDX", 4);
+	ck_assert_int_eq(0xA2, reverse_opcode_lut(ins, IMM));
+	strncpy((char*) ins, "CLV", 4);
+	ck_assert_int_eq(0xB8, reverse_opcode_lut(ins, IMP));
+	strncpy((char*) ins, "CPY", 4);
+	ck_assert_int_eq(0xCC, reverse_opcode_lut(ins, ABS));
+	strncpy((char*) ins, "DEC", 4);
+	ck_assert_int_eq(0xDE, reverse_opcode_lut(ins, ABSX));
+	strncpy((char*) ins, "SBC", 4);
+	ck_assert_int_eq(0xE1, reverse_opcode_lut(ins, INDX));
+	strncpy((char*) ins, "INC", 4);
+	ck_assert_int_eq(0xFE, reverse_opcode_lut(ins, ABSX));
 }
 END_TEST
 
