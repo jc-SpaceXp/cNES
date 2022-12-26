02837ddb5fec71c2abe5d1e25b08ab872f876b55
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2022-12-26 17:21:03 +0000

    Added error handling when initialising a SDL window
    - Previously, there was no error handling when using the SDL functions

--- src/gui.c
+++ src/gui.c
@@ -19,16 +19,44 @@ Display* screen_init(int scale_factor)
 	nes->renderer = NULL;
 	nes->framebuffer = NULL;
 
-	SDL_Init(SDL_INIT_VIDEO);
-	nes->window = SDL_CreateWindow("cNES", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-	              SCREEN_WIDTH * scale_factor, SCREEN_HEIGHT * scale_factor, SDL_WINDOW_SHOWN);
-	nes->renderer = SDL_CreateRenderer(nes->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	nes->framebuffer = SDL_CreateTexture(nes->renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		fprintf(stderr, "Failed to initialise the SDL library: %s\n", SDL_GetError());
+	}
+
+	nes->window = SDL_CreateWindow("cNES", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED
+	                              , SCREEN_WIDTH * scale_factor
+	                              , SCREEN_HEIGHT * scale_factor, SDL_WINDOW_SHOWN);
+	if (nes->window == NULL) {
+		fprintf(stderr, "SDL failed to create window: %s\n", SDL_GetError());
+	}
+
+	nes->renderer = SDL_CreateRenderer(nes->window, -1
+	                                  , SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (nes->renderer == NULL) {
+		fprintf(stderr, "SDL failed to create 2D renderer for the window: %s\n", SDL_GetError());
+	}
+
+	nes->framebuffer = SDL_CreateTexture(nes->renderer, SDL_PIXELFORMAT_ARGB8888
+	                                    , SDL_TEXTUREACCESS_STREAMING
+	                                    , SCREEN_WIDTH, SCREEN_HEIGHT);
+	if (nes->framebuffer == NULL) {
+		fprintf(stderr, "SDL failed to create a texture for the window: %s\n", SDL_GetError());
+	}
 
 	// Apply scaling factor
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
-	SDL_RenderSetLogicalSize(nes->renderer, SCREEN_WIDTH, SCREEN_HEIGHT); // preserve original aspect ratio
-	SDL_RenderSetScale(nes->renderer, scale_factor, scale_factor);
+	if (SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest") == SDL_FALSE) {
+		fprintf(stderr, "SDL failed to set a scaling method/quality for the renderer: %s\n", SDL_GetError());
+	}
+
+	// preserve original aspect ratio
+	if (SDL_RenderSetLogicalSize(nes->renderer, SCREEN_WIDTH, SCREEN_HEIGHT)) {
+		fprintf(stderr, "SDL failed to set a new resolution for the renderer: %s\n", SDL_GetError());
+	}
+
+	if (SDL_RenderSetScale(nes->renderer, scale_factor, scale_factor)) {
+		fprintf(stderr, "SDL failed to apply a scaling factor to the renderer: %s\n", SDL_GetError());
+	}
+
 	return nes;
 }
 
